51b23dba9cf5dedd4b540bb0042d9c25b4237244
EvanMPutnam <emput1998@gmail.com>
2020-04-05 15:57:25 -0400

    Update conf.lua
    Removing extra spacing.

--- conf.lua
+++ conf.lua
@@ -1,8 +1,6 @@
 --  Author:         Evan Putnam
 --  Description:    A configuration file that sets the screen data.
 
-
-
 function love.conf(t)
     t.window.width = 600
     t.window.height = 600
